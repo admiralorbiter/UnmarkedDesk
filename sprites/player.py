617bb2dd541ee98858fb05ef7357b70a9910a765
Jonathan Lane <admiralorbiter@gmail.com>
2024-10-25 10:13:45 -0500

    working on animation

--- sprites/player.py
+++ sprites/player.py
@@ -1,30 +1,63 @@
 import pygame
+import os
 from settings import PLAYER_SPEED, BLUE
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(BLUE)
+        self.load_animations()
+        self.image = self.animations['idle_down'][0]
         self.rect = self.image.get_rect(center=pos)
+        self.direction = pygame.math.Vector2()
         self.speed = PLAYER_SPEED
+        self.current_animation = 'idle_down'
+        self.animation_index = 0
+        self.animation_speed = 0.1
 
-    def handle_keys(self):
-        keys = pygame.key.get_pressed()
-        dx = dy = 0
-
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            dx = -self.speed
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            dx = self.speed
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            dy = -self.speed
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            dy = self.speed
-
-        self.rect.x += dx
-        self.rect.y += dy
+    def load_animations(self):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        images_dir = os.path.join(base_dir, 'images')
+        self.animations = {
+            'idle_down': [pygame.image.load(os.path.join(images_dir, 'down-standing.gif'))],
+            'idle_up': [pygame.image.load(os.path.join(images_dir, 'up-standing.gif'))],
+            'idle_left': [pygame.image.load(os.path.join(images_dir, 'left-standing.gif'))],
+            'idle_right': [pygame.image.load(os.path.join(images_dir, 'right-standing.gif'))],
+            'run_down': [pygame.image.load(os.path.join(images_dir, 'down-running.gif'))],
+            'run_up': [pygame.image.load(os.path.join(images_dir, 'up-running.gif'))],
+            'run_left': [pygame.image.load(os.path.join(images_dir, 'left-running.gif'))],
+            'run_right': [pygame.image.load(os.path.join(images_dir, 'right-running.gif'))],
+        }
 
     def update(self):
-        self.handle_keys()
+        self.move()
+        self.animate()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        self.direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.center += self.direction * self.speed
+
+    def animate(self):
+        if self.direction.magnitude() == 0:
+            if 'run' in self.current_animation:
+                self.current_animation = f'idle_{self.current_animation.split("_")[1]}'
+        else:
+            if self.direction.x > 0:
+                self.current_animation = 'run_right'
+            elif self.direction.x < 0:
+                self.current_animation = 'run_left'
+            elif self.direction.y > 0:
+                self.current_animation = 'run_down'
+            elif self.direction.y < 0:
+                self.current_animation = 'run_up'
+        self.animation_index += self.animation_speed
+        if self.animation_index >= len(self.animations[self.current_animation]):
+            self.animation_index = 0
+
+        self.image = self.animations[self.current_animation][int(self.animation_index)]
 
